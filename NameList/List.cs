b3395b76d9a3386a3e3236145fc83423f3cc7f87
Shin <Shin@DESKTOP-C0QRAI1>
2023-06-15 04:22:54 +0700

    Добавьте файлы проекта.

---
+++ NameList/List.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NameList
+{
+    class List<T> : IEnumerable<T>
+    {
+        Node<T>? head; 
+        Node<T>? tail; 
+        int count;
+
+        public void Add(T data)
+        {
+            Node<T> node = new Node<T>(data);
+
+            if (head == null)
+                head = node;
+            else
+                tail!.Next = node;
+            tail = node;
+
+            count++;
+        }
+
+        public bool Remove(T data)
+        {
+            Node<T>? current = head;
+            Node<T>? previous = null;
+
+            while (current != null && current.Data != null)
+            {
+                if (current.Data.Equals(data))
+                {
+                    if (previous != null)
+                    {
+                        previous.Next = current.Next;
+
+                        if (current.Next == null)
+                            tail = previous;
+                    }
+                    else
+                    {
+                        head = head?.Next;
+
+                        if (head == null)
+                            tail = null;
+                    }
+                    count--;
+                    return true;
+                }
+
+                previous = current;
+                current = current.Next;
+            }
+            return false;
+        }
+
+        public void Contains(T data)
+        {
+            int n = 0;
+            Node<T>? current = head;
+            while (current != null && current.Data != null)
+            {
+                if (current.Data.Equals(data))
+                {
+                    Console.WriteLine(data);
+                    return;
+                }
+                current = current.Next;
+            }
+            Console.WriteLine(0);
+        }
+        public int Count { get { return count; } }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            Node<T>? current = head;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable<T>)this).GetEnumerator();
+        }
+    }
+}
---
+++ NameList/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace NameList
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<string> list = new List<string>();
+            Console.WriteLine("Добавьте имена через пробел: ");
+            string name = Console.ReadLine();
+            string[] names = name.Split(' ');
+
+            foreach (string n in names)
+            {
+                list.Add(n);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Введите имя, которое хотите найти: ");
+            list.Contains(Console.ReadLine());
+
+        }
+    }
+}
